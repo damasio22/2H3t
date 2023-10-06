1e26866c48788281830b38ad27fe369799864181
Maria Damasio <108935070+damasio22@users.noreply.github.com>
2023-10-06 08:10:02 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,25 +1,54 @@
-
-clicado = () => alert("Alô mundo");
-mensagem = (msg) => console.log(msg)
+let num1 = "";
+let num2 = "";
+let valor = "";
+let temPonto = false;
+let executa = "";
+mensagem = (msg) => console.log(msg);
 soma = (a,b) => a + b;
 sub = (a,b) => a - b;
 mult = (a,b) => a * b;
 div = (a,b) => a / b;
-juros_simples = (j,c) => mult(c,div(j,100));
+juros_simples = (c,j) => mult(c,div(j,100));
 montante_juro_simples = (c,j) => soma(juros_simples(c,j),c);
 
-mensagem(soma(4,soma(7,soma(11,15))));
+mostrar_display = (msg)=>{
+  document.getElementById("resultado").value = msg;
+}
 
-mensagem(sub(18,7));
+function digitando(tecla){
+  if(tecla == "."){
+    if(!temPonto){
+      valor += tecla;
+      mostrar_display(valor);
+      temPonto = true;
+    }
+    return;
+  }
+   valor += tecla;
+   mostrar_display(valor);
+}
+function operacao(op){
+  executa = op;
+  num1= valor;
+  valor= "";
 
-mensagem(mult(5,5));
+}
+function calcula(){
+  if(executa != ""){
+    num2 = valor;
+    if(executa == "soma") mostrar_display(soma(Number(num1)),Number(num2));
+    if(executa == "sub") mostrar_display(sub(num1,num2));
+    if(executa == "div") mostrar_display(div(num1,num2));
+    if(executa == "mult") mostrar_display(mult(num1,num2));
+    num1 = "";
+    num2= "";
+    valor= "";
+    temPonto= false;
+    executa= "";
 
-mensagem(div(49,7));
+  }
+}
 
-mensagem(soma(2,div(2,2)));
 
-mensagem(mult(420,div(18,100)));
 
-mensagem(juros_simples(420,18));
 
-mensagem(montante_juro_simples(420,18));
